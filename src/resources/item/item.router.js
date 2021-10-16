41d7c26e9c4bb81502d64e2f7ffae93a7ff2172e
SanjoyPator <sanjoy18-378@aec.ac.in>
2021-10-16 16:12:36 +0530

    sol 2 added first

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,24 @@
 import { Router } from 'express'
 
+// creating controller
+const controller = (req, res) => {
+  res.send({ message: 'hello' })
+}
+
 const router = Router()
 
+// /api/item equivalent
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+// /api/item/:id equivalent
+router
+  .route('/')
+  .put(controller)
+  .delete(controller)
+  .get(controller)
+
+// exporting as default so I can import as anything
 export default router
